af564f804c18cfb843b71f77c0799cc164df8f03
EldiegoGDL <diegoner.dg@gmail.com>
2026-08-27 16:40:32 -0600

    Menu y clase Producto

---
+++ Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArticulosCRUD
+{
+    internal class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Precio { get; set; }
+
+
+
+
+
+    }
+}
